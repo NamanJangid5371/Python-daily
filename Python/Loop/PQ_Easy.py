a8c17d818094b12d6517b02330b26b70e8d09150
Naman <namanjangid5371@gmail.com>
2026-06-03 17:57:43 +0530

    update

--- Python/Loop/PQ_Easy.py
+++ Python/Loop/PQ_Easy.py
@@ -34,4 +34,3 @@
 # name = "Naman"
 # for i in range( 0,len(name)):
 #     print(name[i])
- 
---
+++ Python/Loop/PQ_Medium.py
@@ -0,0 +1,19 @@
+#factorial of a number.
+# num = int(input("Enter the number for factorial: "))
+# factorial = 1
+
+# for i in range(1, num+1):
+# 	factorial *= i
+
+# print("Factorial =", factorial)
+
+#reverse a number using a loop
+# num = int(input("Enter a number: "))
+# reverse=0
+
+# while num>0:
+#     digit = num % 10
+#     reverse = reverse *10 + digit
+#     num = num //10
+# print("Reverse number:", reverse)
+
