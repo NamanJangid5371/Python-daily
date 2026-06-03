c0979eaf22f83afeb3b316964300e0be17e6d4a3
Naman <namanjangid5371@gmail.com>
2026-06-03 17:24:35 +0530

    loop

---
+++ Python/Loop/PQ_Easy.py
@@ -0,0 +1,37 @@
+#inverse numbers 
+# for i in range (10,0,-1):
+#     print(i)
+
+#all even number between 1 to 50
+# for i in range (1,61):
+#  if i%2==0:
+#   print(i)
+
+#all odd number between 1 to 50
+
+# for i in range (1,61):
+#     if i%2!=0:
+#         print(i)
+
+#The sum of numbers from 1 to 100
+# sum=0
+# for i in range (1,100):
+#     sum = i + sum
+#     print(sum)
+
+#multiplication table
+# num = int(input("Enter the number: "))
+# for i in range (1,11):
+#     print(f"{num}x{i}={num*i}")
+
+#Count how many numbers are there between 1 and 100
+# count=0
+# for i in range(1,101):
+#     count+=1
+# print("Total number: ",count)
+
+#character of string using a
+# name = "Naman"
+# for i in range( 0,len(name)):
+#     print(name[i])
+ 
