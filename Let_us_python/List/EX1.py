161c91728dce0129a4df3e6901d6dc0e56183bf8
Naman <namanjangid5371@gmail.com>
2026-06-04 15:26:08 +0530

    new

---
+++ Let_us_python/List/EX1.py
@@ -0,0 +1,12 @@
+animals = ['Zebra','Tiger','Lion','Jackal','Kangaroo']
+
+# i = 0 
+
+# while i<len(animals):
+#     print(animals[i])
+#     i+=1
+# for a in animals:
+#     print(a)
+
+for index, a in enumerate(animals):
+    print(index,a)
