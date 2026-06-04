9c633963ee27dd985c9f66639b7e0642982a3bb7
Naman <namanjangid5371@gmail.com>
2026-06-04 14:44:03 +0530

    new

--- Let_us_python/input_output console/shoping.py
+++ Let_us_python/input_output console/shoping.py
@@ -6,4 +6,4 @@ iprice = float(input("Enter the price of iteam: "))
 
 total_cost = qiteam*iprice
 
-print("Recept",iname ,f"qty:{qiteam}",f"total: ${total_cost:.2f}",sep="|")
+print("Recept",iname ,f"qty:{qiteam}",f"total: ₹{total_cost:.2f}",sep="|")
