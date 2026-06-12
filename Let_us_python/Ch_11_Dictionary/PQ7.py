9f579ad29aaa1aace225b891122f7fc3a82fc0b4
Naman <namanjangid5371@gmail.com>
2026-06-12 17:25:36 +0530

    new file

---
+++ Let_us_python/Ch_11_Dictionary/PQ7.py
@@ -0,0 +1,8 @@
+capitals = {"India":"Delhi",
+            "Pakistan":"Islamabad",
+            "Shri lanka":"Sri Jayawardenepura Kotte",
+            "Nepal":"Kathmandu",
+            "China":"beijing",
+            "Bangladesh":"Dhaka"}
+
+print("No. of keys in dictionary: ",len(capitals.values()))
