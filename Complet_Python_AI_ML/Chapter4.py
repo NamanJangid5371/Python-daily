7201300b76b16bc795a001148995e01b48ca2f0c
Naman <namanjangid5371@gmail.com>
2026-07-01 16:56:01 +0530

    new learning

---
+++ Complet_Python_AI_ML/Chapter4.py
@@ -0,0 +1,17 @@
+# a = "N"
+# print(ord(a))
+
+#slicing
+# a = "Colleg"
+# print(a[3:6:1])
+
+# a = "Hello how are you"
+
+# print(a[6:10:1]) #print how 
+
+# print(a[-4::1]) #print you
+
+# print(a[0:6:1])
+
+#Type conversion 
+
---
+++ Complet_Python_AI_ML/Chapter5.py
@@ -0,0 +1,6 @@
+# for i in range(1,11,1):
+#     print(5*i)
+a = 1 
+while True:
+    print(a)
+    a +=1
