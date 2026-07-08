84e1c901db1f740f85334d5e6099c9be0838e888
Naman <namanjangid5371@gmail.com>
2026-07-08 14:33:41 +0530

    new file

--- Practice Python/Basics of Python/FileHandling.py
+++ Practice Python/Basics of Python/FileHandling.py
@@ -1,7 +1,13 @@
 # open("hello.txt","x") #create a new file 
 
-file = open("New.txt","w")
+# file = open("New.txt","w")
 
-data = input("Waht you want to wirte in the file :- ")
+# data = input("Waht you want to wirte in the file :- ")
 
-file.write(data)
+# file.write(data)
+
+# file = open("number_Guss_game.py","r")
+# print(file.read())
+
+with open("New.txt","a") as f:
+    f.write(" " + "i want to see if it is working or not")
