464d0b97666c83f32ad2fd3b0026e99840857e2b
Naman <namanjangid5371@gmail.com>
2026-07-12 15:18:30 +0530

    new update

--- Complet_Python_AI_ML/Practice_game.py
+++ Complet_Python_AI_ML/Practice_game.py
@@ -1,11 +1,14 @@
 import random
 com = random.randint(1,100)
 tries = 0 
+atempt = 0 
 
 while True:
-    
+    atempt = atempt + 1 
     tries = tries + 1
     hum = int(input("guees your number between 1 - 100 :-"))
+    print(f"This is your atempt number {atempt}")
+
 
     if hum == com:
         print(f"congratulations you have won in {tries} tries!")
@@ -15,4 +18,4 @@ while True:
         print("sorry wrong guess go lower !")
 
     elif hum < com:
-        print("sorry wrong guess go higher !")
+        print("sorry wrong guess go higher ! ")
