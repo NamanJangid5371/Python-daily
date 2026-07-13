28b6dc2875afbcc4efca253a1b0fdf209dfc0148
Naman <namanjangid5371@gmail.com>
2026-07-13 12:06:13 +0530

    New learinging

---
+++ Complet_Python_AI_ML/Chapter16.py
@@ -0,0 +1,13 @@
+a = int(input("Please tell your number 1: "))
+b = int(input("Please tell your number 2: "))
+try:    
+    print(a/b)
+except Exception as err:
+    print(f"Sorry and error acured as {err}")
+else:
+    print("No error accured.")
+finally:
+    print("There are error or there are no errors I will run no matter what.")
+
+name = input("tell your name:-")
+print(f"Your name is {name}") 
