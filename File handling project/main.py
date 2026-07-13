6002a7a4a1a993d0ea2a93f79942fb8fc7f18f34
Naman <namanjangid5371@gmail.com>
2026-07-13 15:02:34 +0530

    new file

--- File handling project/main.py
+++ File handling project/main.py
@@ -10,9 +10,9 @@ def createfile():
             with open(path,"w") as fs:
                 data = input("What you want to write:-")
                 fs.write(data)
-                print("Erro file name is alredy exists.")
+                print("Done file created successfully.")
         else:
-            print("Error file created successfully.")
+            print("Error file alread exists.")
 
     except Exception as err:
         print(f"an error occured as {err}")
@@ -21,7 +21,7 @@ def readfile():
     try:
         name = input("Please tell your file name :-")
         path = Path(name)
-        if path.exists:
+        if path.exists():
             with open(path,"r") as fs:
                 content = fs.read()
                 print(f"your file content is \n {content}")
@@ -33,21 +33,55 @@ def readfile():
 
 
 def updatefile():
-    name = input("Please tell your file name :- ")
-    path = Path(name)
+    try:
+        name = input("Please tell your file name :- ")
+        path = Path(name)
 
-    if path.exists():
-        print("operations ")
-        print("1 . Renaming the file ")
-        print("2 . Appending the content")
-        print("3 . Overwirting the file")
-        
-    if path.exists: 
-        with open(path,"r") as fs:
-            content = fs.read()
-            print(f"change the file name to this:{}")
-def deletfile():
-    pass
+        if path.exists():
+            print("operations ")
+            print("1 . Renaming the file ")
+            print("2 . Appending the content")
+            print("3 . Overwirting the file")
+
+            choice = int(input("Enter your option :-"))
+
+            if choice == 1:
+                newname = input("tell your new file name:- ")
+                new_path = Path(newname)
+                if not new_path.exists():
+                    path.rename(new_path)
+                    print("renamed successfully")
+                else:
+                    print("file already exists")
+
+            elif choice == 2:
+                with open(path,'a') as fs:
+                    data = input("what do you want to append :-")
+                    fs.write(" \n"+data)
+                print("successfully appended")
+            elif choice == 3:
+                with open(path, 'w') as fs:
+                    data = input("what do you want to append :-")
+                    fs.write(" \n"+data)
+                print("successfully appended")
+
+    except Exception as err:
+        print(f"An error occured as {err}")
+
+
+
+def deletefile():
+    try:
+        name = input("Please tell your file name:- ")
+        path = Path(name)
+        if path.exists():
+            path.unlink()
+            print("file deleted succesfully.")
+        else:
+            print("no such file exists.")
+    except Exception as err:
+        print(f"An error occured as {err}")
+
 
 
 print("press 1 for creating a file.")
@@ -61,8 +95,8 @@ if a == 1:
     createfile()
 if a == 2: 
     readfile()
-if a == 3: 
+if a == 3:
     updatefile()
-if a == 4: 
-    deletfile()
+if a == 4:
+    deletefile()
 
