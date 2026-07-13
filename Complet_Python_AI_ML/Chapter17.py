68cd93e3f7a099d852594f75cd20cb4915c58384
Naman <namanjangid5371@gmail.com>
2026-07-13 14:24:22 +0530

    new file

---
+++ Complet_Python_AI_ML/Chapter17.py
@@ -0,0 +1,10 @@
+# open("Naman.txt","x")
+# file = open("superman.txt","w")
+# data = input("What you want to writ in your file.")
+# file.write(data)
+# file = open("Practice_game.py","r")
+
+# print(file.read())
+
+with open("Superman.txt","a") as f:
+    f.write(" "+"i want to see if it is working or not.")
